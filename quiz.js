fe43423b34dec28e7607f8236c562b14afd476cd
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2021-09-30 02:39:06 +0530

    added basic html and i/o

---
+++ quiz.js
@@ -0,0 +1,5 @@
+var quizForm = document.querySelector(".questions");
+var submitbtn = document.querySelector("#submit-answer");
+var score = document.querySelector("#output");
+ 
+var answer = ["true","yes"];
