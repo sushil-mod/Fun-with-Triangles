4eb672e9a7026fb6749240dca3d5a966b023ef14
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2021-09-30 17:03:52 +0530

    style: added styling

--- area.js
+++ area.js
@@ -5,7 +5,7 @@ var outputArea = document.querySelector("#output");
 areaBtn.addEventListener('click',calculateArea)
 
 function calculateArea(){
-    if()
+    
     var area = (1/2)*Number(sideInput[0].value)*Number(sideInput[1].value);
     outputArea.innerText = "Area of a triangle is " + area ;
 }
