7475328348fe6f2fad485a65ad2e1fa7271875af
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2021-10-06 18:47:40 +0530

    minor changes which i have forget to do

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ var quizForm = document.querySelector(".quiz-form");
 var submitBtn = document.querySelector("#submit-answer");
 var outputScore = document.querySelector("#output");
  
-var answer = ["true","yes"];
+var answer = ["true","yes","5","6","45","6","30"];
 
 submitBtn.addEventListener('click',clickHandlerForm)
 
